14d1d754b0af81cb2f35b135b433804243297060
Samara Flor <samaraflor2013>
2023-01-09 15:56:59 -0300

    testes em node

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const somarHorasExtras = (salario, valorHoraExtras) => salario + valorHoraExtras;
+
+const calcularDesconto =(salario, desconto)=> salario - desconto;
+
+export{
+    somarHorasExtras,
+    calcularDesconto
+};
